9d252e962806094777656144074d9e7c173ce33c
Tyler Dolph <tdolph0016@myduneland.org>
2021-05-12 23:55:50 -0500

    Update Quake_me_baby_one_more_time.java

--- Quake_me_baby_one_more_time.java
+++ Quake_me_baby_one_more_time.java
@@ -3,15 +3,20 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.KeyAdapter;              
+import java.awt.event.KeyEvent;
+
 
 public class Quake_me_baby_one_more_time{
 //=============================================
     static int[][] grid = new int[25][25]; //grid that makes up the gameplay area
     static int[] windowSize = {600,600};
     static Color[] colors = new Color[]{Color.black, Color.orange, Color.blue};
+    public static Player player = new Player(windowSize[0]/2f, windowSize[1]/2f,0f);
 //=============================================
     public static void main(String[] args){
-        grid[5][7]=1;
+        grid[3][15]=1;
         JFrame frame = new JFrame();
         Container pane = frame.getContentPane();
         pane.setLayout(new BorderLayout());
@@ -29,35 +34,49 @@ public class Quake_me_baby_one_more_time{
         frame.setVisible(true);
     }
 //=============================================
-static void draw(Graphics graphics){
-    Graphics2D g2d = (Graphics2D) graphics;
-    int gridSize = 20;//windowSize[0]/grid.length;
-    //=============================================
-    for(int y = 0; y < grid.length; y++){
-        for(int x = 0; x < grid[y].length; x++){
-            int col = grid[y][x];
-            if(col>=1){ //zero is no block, so draw one if it's one or greater
-                g2d.setColor(colors[col]); //set the block color to it's value in colors
-                g2d.setStroke(new BasicStroke(gridSize)); //set line thiccness to gridsize
-                g2d.drawRect((x*gridSize)+(gridSize/2), (y*gridSize)+(gridSize/2), 0,0);
-                //the block size is zero, so it draws a filled square of the stroke size
-            }
-            //drawing the gridline
-            g2d.setColor(colors[0]); //set color to black.
-            g2d.setStroke(new BasicStroke(1f)); //set line thiccness
-            g2d.drawRect(x*gridSize, y*gridSize, gridSize, gridSize);//draw gridline
+    public static void draw(Graphics graphics){
+        Graphics2D g = (Graphics2D) graphics;
+        int gridSize = 20;//windowSize[0]/grid.length;
+        //draw the map ================================
+        for(int y = 0; y < grid.length; y++){
+            for(int x = 0; x < grid[y].length; x++){
+                int col = grid[y][x];
+                if(col>=1){ //zero is no block, so draw one if it's one or greater
+                    g.setColor(colors[col]); //set the block color to it's value in colors
+                    g.setStroke(new BasicStroke(gridSize)); //set line thiccness to gridsize
+                    g.drawRect((x*gridSize)+(gridSize/2), (y*gridSize)+(gridSize/2), 0,0);
+                    //the block size is zero, so it draws a filled square of the stroke size
+                }
+                //drawing the gridline
+                g.setColor(colors[0]); //set color to black.
+                g.setStroke(new BasicStroke(1f)); //set line thiccness
+                g.drawRect(x*gridSize, y*gridSize, gridSize, gridSize);//draw gridline
             }
         }
+        g.setColor(colors[2]);
+        g.setStroke(new BasicStroke(3));
+        g.drawRect((int)player.x(), (int)player.y(), 5, 5);
+        //g.drawRoundRect((int)player.x(), (int)player.y(), 20, 20, 20, 20);
+
     }
 //=============================================
-    class Player{
-        double x;
-        double y;
-        double rot;
-        Player(double x,double y,double rot){
+    public static class Player{
+        public float x; // x coordinate
+        public float y; //y coordinate
+        public float rot; //rotation
+        Player(float x,float y,float rot){
             this.x=x;
             this.y=y;
             this.rot=rot;
+        }
+        public float x(){return x;}
+        public float y(){return y;}
+        public float rot(){return rot;}
+    }
+//=============================================
+    public class KeyInput extends KeyAdapter {
+        public void keyPressed(KeyEvent d){
+            //do stuff if the d key is pressed
         }
     }
 //=============================================
