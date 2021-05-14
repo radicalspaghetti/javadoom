8027be9ef3a93fe89f5db62a440d8c9a34d3679e
Tyler Dolph <tdolph0016@myduneland.org>
2021-05-13 22:05:10 -0500

    asdbvawdbZSd

--- Quake_me_baby_one_more_time.java
+++ Quake_me_baby_one_more_time.java
@@ -17,18 +17,20 @@ public class Quake_me_baby_one_more_time{
 //=============================================
     static int[][] grid = new int[25][25]; //grid that makes up the gameplay area
     static int[] windowSize = {517,540};
-    static Color[] colors = new Color[]{Color.black, Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.pink};
+    static Color[] colors = new Color[]{Color.black, Color.orange, Color.pink, Color.yellow, Color.cyan};
     public static Player player = new Player(windowSize[0]/2f, windowSize[1]/2f,0f);
     public static ArrayList<Integer> keys = new ArrayList<Integer>();
+    public static int gridSize = 20;
 //=============================================
     public static void main(String[] args){
-        grid[3][15]=1;
+        grid[3][15]=2;
         JFrame frame = new JFrame();
         Container pane = frame.getContentPane();
         pane.setLayout(new BorderLayout());
         frame.setSize(windowSize[0],windowSize[1]);
         frame.setTitle("Quake Me Baby One More Time");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBackground(Color.darkGray);
         //keyboard stuff
         KeyListener input = new Input();
 		frame.addKeyListener(input);
@@ -38,39 +40,75 @@ public class Quake_me_baby_one_more_time{
             public void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g;
                 draw(g2);
-
             }
         };
         pane.add(renderPanel, BorderLayout.CENTER);
         frame.setVisible(true);
 
-        int y = 0;
-        int x = 0;
-        int z = 1;
+        //main loop
+        float rotationSpeed = .00000085f;
         while(true){
-            if(keys.contains(39)){
-                player.rot-=.2;
+            if(keys.contains(37)||keys.contains(65)){ //right rotation
+                player.rot-=rotationSpeed;
                 if(player.rot<.0001){player.rot+=Math.PI*2;}
                 player.deltaX=(float)(Math.cos(player.rot)*5); 
                 player.deltaY=(float)(Math.sin(player.rot)*5); 
             }
-            if(x>=grid[y].length){y+=1; x=0;}
-            if(y>=grid.length){y=0; x=0;} //z=z+1;}
-            if(keys.size()>0){z+=1;}
-            if(z>=colors.length){z=1;}
-            grid[y][x]=z;
-            x++;
+            if(keys.contains(39)||keys.contains(68)){ //left rotation
+                player.rot+=rotationSpeed;
+                if(player.rot>Math.PI*2){player.rot-=Math.PI*2;}
+                player.deltaX=(float)(Math.cos(player.rot)*5); 
+                player.deltaY=(float)(Math.sin(player.rot)*5); 
+            }
+            //TODO movement
            // System.out.println(keys.toString() +"  "+ player.rot);
            //System.out.println(frame.getHeight()+" "+frame.getWidth());
             renderPanel.removeAll();
+            renderPanel.revalidate();
             renderPanel.repaint();
         }
     }
 //=============================================
+//note: current rendering code should be recycled for minimap
     public static void draw(Graphics graphics){
+        //draw the background
         Graphics2D g = (Graphics2D) graphics;
-        int gridSize = 20;//windowSize[0]/grid.length;
-        //draw the map ================================
+        g.setColor(Color.darkGray);
+        g.setStroke(new BasicStroke(520));
+        g.drawRect(0,0,windowSize[0],windowSize[1]);
+        //draw the main window here ================================
+        double rayRot = player.rot;
+        int dof, mx, my;
+        int dofMax = 8;
+        double rayX, rayY, xOffset, yOffset;
+        for(int r=0; r<1; r++){ //raycasting time babeyyyyyy
+            //horizontal gridlines ================================
+            dof=0;
+            double aTan = -1/Math.tan(rayRot);
+            if(rayRot==0||rayRot==Math.PI){rayX=player.x; rayY=player.y; dof=dofMax;}//looking directly right or left
+            if(rayRot>Math.PI){ //looking up
+                rayY=((((int)player.y>>6)<<6)-0.0001); //evil bitshifting. how vile. also it breaks everything. //TODO replace bitshifting
+                rayX=(player.y-rayY)*aTan+player.x;
+                yOffset=-gridSize; xOffset=-yOffset*aTan;
+            }
+            else{ //looking down
+                rayY=((((int)player.y>>6)<<6)+gridSize); //here too.
+                rayX=(player.y-rayY)*aTan+player.x;
+                yOffset=gridSize; xOffset=-yOffset*aTan;
+            }
+            while(dof<dofMax){
+                mx=(int)(rayX)>>6; my=(int)(rayY)>>6;
+                if(my<grid.length&&mx<grid[my].length&&grid[my][mx]>0){dof=dofMax;} //hit a wall :D
+                else{rayX+=xOffset; rayY+=yOffset; dof+=1;} //didn't hit a wall, add offsets and inumerate dof
+            }
+            g.setColor(Color.orange);
+            g.setStroke(new BasicStroke(1));
+            g.drawLine((int)player.x,(int)player.y,(int)rayX,(int)rayY);
+        }
+        //================================
+        drawMap(g);
+    }
+    public static void drawMap(Graphics2D g){
         for(int y = 0; y < grid.length; y++){
             for(int x = 0; x < grid[y].length; x++){
                 int col = grid[y][x];
@@ -80,36 +118,39 @@ public class Quake_me_baby_one_more_time{
                     g.drawRect((x*gridSize)+(gridSize/2), (y*gridSize)+(gridSize/2), 0,0);
                     //the block size is zero, so it draws a filled square of the stroke size
                 }
-                //drawing the gridline
+                //drawing the gridlines
                 g.setColor(colors[0]); //set color to black.
                 g.setStroke(new BasicStroke(1f)); //set line thiccness
                 g.drawRect(x*gridSize, y*gridSize, gridSize, gridSize);//draw gridline
             }
         }
-        g.setColor(colors[2]);
-        g.setStroke(new BasicStroke(3));
-        //g.drawRect((int)player.x(), (int)player.y(), 5, 5); //draw the player
+        //draw the player's direction line =========================
+        int line = 10;
+        g.setColor(colors[4]);
+        g.setStroke(new BasicStroke(2));
+        g.drawLine((int)player.x,(int)player.y,(int)(player.x+player.deltaX*line),(int)(player.y+player.deltaY*line));
+        //draw the player on the map================================
+        g.setColor(colors[3]);
+        g.setStroke(new BasicStroke(10));
+        g.drawRect((int)player.x, (int)player.y, 0, 0); //draw the player
     }
 //=============================================
-    public static class Player{
+    public static class Player{ //TODO: move player-related code to the player class to avoid spaghetti
         public float x; // x coordinate
         public float y; //y coordinate
         public float rot; //rotation
-        public float deltaX = 0;
-        public float deltaY = 0;
+        public float deltaX = 1;
+        public float deltaY = 1;
         Player(float x,float y,float rot){
             this.x=x;
             this.y=y;
             this.rot=rot;
         }
-        public float x(){return x;}
-        public float y(){return y;}
-        public float rot(){return rot;}
     }
 //=============================================
     public static class Input implements KeyListener {
         @Override
-        public void keyTyped(KeyEvent e) {}//balls
+        public void keyTyped(KeyEvent e) {}//balls uwu (I don't even know what this does. it is mysterious.)
         @Override
         public void keyPressed(KeyEvent e) {
             //System.out.println("key pressed: "+e.getKeyCode());
